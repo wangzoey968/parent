22f8e5125d53d0d38139c968661cfbc2994cae41
wangzoey968 <1324633295@qq.com>
2018-04-24 14:47:16 +0800

    video

--- manage_web/src/main/java/com/it/controller/UserController.java
+++ manage_web/src/main/java/com/it/controller/UserController.java
@@ -4,30 +4,24 @@ import com.it.model.Tb_User;
 import com.it.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -41,14 +35,31 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping("/userInfo")
-    public String userInfo(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") Integer id, Model model) {
+    public String userInfo(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") Integer id, Model model,
+                           @CookieValue(value = "sessionId", required = true) String sessionId) {
         List<Tb_User> tb_Users = userService.selectUser(id);
         model.addAttribute("users", tb_Users);
+        //return "redirect:/jsp/user/aaa.html";
         return "/jsp/user/userInfo";
     }
 
-    @RequestMapping(value = "/userRole")
+    @RequestMapping("/userCookie")
+    public void userCookie(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") Integer id) {
+        List<Tb_User> user = userService.selectUser(id);
+        response.addCookie(new Cookie("user", user.toString()));
+
+    }
+
+    //将图片写入到输出流中,客户端读取到图片
     @ResponseBody
+    @RequestMapping("/userImg")
+    public byte[] userImg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Resource resource = new ClassPathResource("/headIcon.jpg");
+        byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+        return bytes;
+    }
+
+    @RequestMapping(value = "/userRole")
     public ModelAndView selectUserRole(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") Integer id) {
         ModelAndView mv = new ModelAndView();
         List<Tb_User> tb_Users = userService.selectUser(id);
@@ -61,6 +72,7 @@ public class UserController {
      * 上传文件
      * 自动创建目标文件夹
      * 如果文件夹下已经含有相同文件名的文件,不上传;不同文件名的,则上传
+     *
      * @param req
      * @param resp
      */
